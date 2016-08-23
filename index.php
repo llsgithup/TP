ba2972fd06f34ac621044921635af5f1cdcd5645
llsgithup <343982190@qq.com>
2016-08-23 15:43:09 +0800

    second

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+// 应用入口文件
+
+
+
+// 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
+define('APP_DEBUG',True);
+
+// 定义应用目录
+define('APP_PATH','./ls/');
+//define('APP_PATH','./myadmin/');
+// 引入ThinkPHP入口文件
+require './ThinkPHP/ThinkPHP.php';
+
+// 亲^_^ 后面不需要任何代码了 就是如此简单
